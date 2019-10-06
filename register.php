705ea1e0bd5fb9a77102f3f0f204baf135f5c165
Fredrik Langå <fredriklanga@Fredriks-MacBook-Pro-2.local>
2019-10-06 22:12:21 +0200

    adding register form to register.php

--- register.php
+++ register.php
@@ -17,9 +17,42 @@
             </p>
             <p>
                 <label for="loginPassword">Password</label>
-                <input id="loginPassword" name="loginPassword" type="password"  required>
+                <input id="loginPassword" name="loginPassword" type="password" placeholder="Password" required>
             </p>
             <button type="submit" name="loginButton">Login</button>
+        </form>
+
+        <form id="registerForm" action="register.php" method="POST">
+            <h2>Create your free account</h2>
+            <p>
+                <label for="registerUsername">Username</label>
+                <input id="registerUsername" name="registerUsername" type="text" placeholder="username" required>
+            </p>
+            <p>
+                <label for="registerFirstName">First name</label>
+                <input id="registerFirstName" name="registerFirstName" type="text" placeholder="First name" required>
+            </p>
+            <p>
+                <label for="registerLastName">Last name</label>
+                <input id="registerLastName" name="registerLastName" type="text" placeholder="Last name" required>
+            </p>
+            <p>
+                <label for="registerEmail">Email</label>
+                <input id="registerEmail" name="registerEmail" type="email" placeholder="email" required>
+            </p>
+            <p>
+                <label for="registerConfirmEmail">Confirm email</label>
+                <input id="registerConfirmEmail" name="registerConfirmEmail" type="email" placeholder="Confirm email" required>
+            </p>
+            <p>
+                <label for="registerPassword">Password</label>
+                <input id="registerPassword" name="registerPassword" type="password" placeholder="Password" required>
+            </p>
+            <p>
+                <label for="registerConfirmPassword">Confirm Password</label>
+                <input id="registerConfirmPassword" name="registerConfirmPassword" type="password"  required>
+            </p>
+            <button type="submit" name="registerButton">Sign up</button>
         </form>
     </div>
 </body>
