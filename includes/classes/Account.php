6364551324a921f4dcb5cecf7af062c85c82ca0a
Fredrik Langå <fredriklanga@Fredriks-MacBook-Pro-2.local>
2019-10-08 11:24:35 +0200

    adding error messages and remembering input values in forms

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -16,12 +16,27 @@ class Account
         $this->validateLastName($lastName);
         $this->validateEmails($email, $confirmEmail);
         $this->validatePasswords($password, $confirmPassword);
+
+        if (empty($this->errorArray)) {
+            // insert into db
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function getError($error)
+    {
+        if (!in_array($error, $this->errorArray)) {
+            $error = "";
+        }
+        return "<span class='errorMessage'>$error</span";
     }
 
     private function validateUsername($username)
     {
         if (strlen($username) > 25 || strlen($username) < 5) {
-            array_push($this->errorArray, "Your username must be between 5 and 25 characters.");
+            array_push($this->errorArray, Constants::$usernameCharacterLength);
             return;
         }
 
@@ -29,14 +44,47 @@ class Account
     }
 
     private function validateFirstName($firstName)
-    { }
+    {
+        if (strlen($firstName) > 25 || strlen($firstName) < 2) {
+            array_push($this->errorArray, Constants::$firstNameCharacterLength);
+            return;
+        }
+    }
 
     private function validateLastName($lastName)
-    { }
+    {
+        if (strlen($lastName) > 25 || strlen($lastName) < 2) {
+            array_push($this->errorArray, Constants::$lastNameCharacterLength);
+            return;
+        }
+    }
 
     private function validateEmails($email, $confirmEmail)
-    { }
+    {
+        if ($email != $confirmEmail) {
+            array_push($this->errorArray, Constants::$emailsDoNotMatch);
+            return;
+        }
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            array_push($this->errorArray, Constants::$emailIsInvalid);
+        }
+
+        // TODO: check that email hasn't already been used
+    }
 
     private function validatePasswords($password, $confirmPassword)
-    { }
+    {
+        if ($password != $confirmPassword) {
+            array_push($this->errorArray, Constants::$passwordsDoNotMatch);
+            return;
+        }
+        if (preg_match('/[^A-Za-z0-9]/', $password)) {
+            array_push($this->errorArray, Constants::$passwordIsNotAlphaNumeric);
+            return;
+        }
+        if (strlen($password) > 30 || strlen($password) < 5) {
+            array_push($this->errorArray, Constants::$passwordCharacterLength);
+            return;
+        }
+    }
 }
---
+++ includes/classes/Constants.php
@@ -0,0 +1,13 @@
+<?php
+
+class Constants
+{
+    public static $usernameCharacterLength = "Your username must be between 5 and 25 characters";
+    public static $lastNameCharacterLength = "Your last name must be between 5 and 25 characters";
+    public static $firstNameCharacterLength = "Your first name must be between 5 and 25 characters";
+    public static $emailsDoNotMatch = "Your emails do not match";
+    public static $emailIsInvalid = "Your email is invalid";
+    public static $passwordsDoNotMatch = "Your passwords do not match";
+    public static $passwordIsNotAlphaNumeric = "Your password can only contain upper and lowercase letters and numbers";
+    public static $passwordCharacterLength = "Your password must be between 5 and 30 characters";
+}
